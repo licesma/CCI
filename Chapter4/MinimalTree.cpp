56119dffc34c5a9f7d5216cc2772044088671f66
licesma <36255528+licesma@users.noreply.github.com>
2019-08-18 23:53:15 -0500

    added some Chapter 4 problems

---
+++ Chapter4/MinimalTree.cpp
@@ -0,0 +1,27 @@
+
+#include <iostream>
+#include <vector>
+#include "TreeNode.h"
+using namespace std;
+
+
+TreeNode* minimalTree(vector<int> arr) {
+	return minimalTree(arr, 0, arr.size() - 1);
+}
+TreeNode* minimalTree(vector<int> arr, int start, int finish) {
+	if (finish >= start) {
+		int mid = (start + finish) / 2;
+		TreeNode* head; head = new TreeNode(mid);
+		head->left = minimalTree(arr, start, mid - 1);
+		head->right = minimalTree(arr, mid + 1, finish);
+		return head;
+	}
+	else
+		return nullptr;
+}
+
+int main()
+{
+	return 0;
+}
+
